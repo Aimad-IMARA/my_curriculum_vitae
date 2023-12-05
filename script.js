4c9933500dcb5933d33a7122737edad6fb67716b
Aimad-IMARA <aimadimara99@gmail.com>
2023-12-05 14:20:31 +0100

    my cv

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", () => {
+  const verticalScrollPx = window.scrollY || window.pageYOffset;
+  let navbar = document.getElementById("navbar");
+
+  if (verticalScrollPx < 500) {
+    navbar.style.backgroundColor = "transparent";
+  } else {
+    navbar.style.backgroundColor = "rgba(26, 26, 26, 0.900)";
+  }
+  navbar.classList.add("transition-navbar");
+});
+document.getElementById("navbar").addEventListener("transitionend", () => {
+  let navbar = document.getElementById("navbar");
+  navbar.classList.remove("transition-navbar");
+});
